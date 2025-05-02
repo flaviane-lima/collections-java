8fa09fd072e0a02d1964ab17fcf0c9ddd2e3d493
flaviane-lima <rflaviane41@gmail.com>
2025-05-02 13:03:13 -0300

    feat: criação da classe Tarefa

--- src/list/OperacoesBasicas/Tarefa.java
+++ src/list/OperacoesBasicas/Tarefa.java
@@ -1,4 +1,19 @@
 package list.OperacoesBasicas;
 
 public class Tarefa {
+    // vai ter um atributo descrição
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
 }
