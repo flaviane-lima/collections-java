d30015c67cbabc1241c9a82bcf529d8c91040506
flaviane-lima <rflaviane41@gmail.com>
2025-05-02 13:01:48 -0300

    feat: criação da regra de negócio da classe ListaTarefa

--- src/list/OperacoesBasicas/ListaTarefa.java
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -1,4 +1,47 @@
 package list.OperacoesBasicas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaTarefa {
+    //tem um atributo ListaTarefa
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    //metodo adicionar
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    //metodo remover
+    public void removerTarefa(String descrição) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descrição)) {
+                tarefasParaRemover.add(t);
+            }
+
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    //metodo obter numero total de tarefas
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+
+    }
+
+    //metodo para obter descrição de uma tarefa
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefaList);
+    }
+
+
+
 }
+
+
+
