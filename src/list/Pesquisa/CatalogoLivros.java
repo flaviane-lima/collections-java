1a841490974c443f26f9ec6d20f7c2bc044e7c92
flaviane-lima <rflaviane41@gmail.com>
2025-05-02 18:06:13 -0300

    feat(core): criação da classe CataloLivros para regra de negócio

--- src/list/Pesquisa/CatalogoLivros.java
+++ src/list/Pesquisa/CatalogoLivros.java
@@ -1,4 +1,66 @@
 package list.Pesquisa;
 
-public class CataloLivro {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    //atributo
+    private List<Livro> livroList;
+
+    //construtor de livros
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    //metodos adicionar
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+
+    }
+
+    //metodo para pesquisar por autor
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livroPorAutor = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livroPorAutor.add(l);
+                }
+            }
+        }
+        return livroPorAutor;
+
+    }
+
+    //metodo livros por intervalo de tempo
+    public List<Livro> pesquisarPorIntervalosAnos(int anoInicial, int anoFinal) {
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+
+        }
+        return livrosPorIntervaloAnos;
+    }
+
+    //metodo retornando livro
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if (!livroList.isEmpty()) {
+            for (Livro l: livroList) {
+                if(l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+
+
 }
+
