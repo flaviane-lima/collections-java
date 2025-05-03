bf1115161a98ef3c71bb8c61e5ad75ef1bae9e99
flaviane-lima <rflaviane41@gmail.com>
2025-05-02 21:16:45 -0300

    feat(core): criação da classe OrdenacaoPessoa onde contém a regra de negócio

--- src/list/Ordenacao/OrdenacaoPessoa.java
+++ src/list/Ordenacao/OrdenacaoPessoa.java
@@ -1,4 +1,41 @@
 package list.Ordenacao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class OrdenacaoPessoa {
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    //construtor
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    // método adicionar pessoa
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    // método ordenar por idade
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    //método orddenar por altura
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPoraltura());
+        return pessoasPorAltura;
+    }
+
+    public List<Pessoa> getPessoaList() {
+        return pessoaList;
+    }
+
+
 }
