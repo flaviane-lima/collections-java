bb83885ddaed298ac4a63d797cd4d23c1e38e758
flaviane-lima <rflaviane41@gmail.com>
2025-05-03 17:43:46 -0300

    Criação da classe Produto para ter a regra de negócio

--- src/map/Pesquisa/Produto.java
+++ src/map/Pesquisa/Produto.java
@@ -1,4 +1,40 @@
 package map.Pesquisa;
 
 public class Produto {
+    //atributos
+    private String nome;
+
+    private  double preco;
+
+    private int quatidade;
+
+    //construtores
+    public Produto(String nome, double preco, int quatidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quatidade = quatidade;
+    }
+
+    // getters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuatidade() {
+        return quatidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quatidade=" + quatidade +
+                '}';
+    }
 }
