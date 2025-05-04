4cb80ad17ad8f164c3e25640560787b8f77b4ffc
flaviane-lima <rflaviane41@gmail.com>
2025-05-03 22:06:24 -0300

    criação da classe Ordenação onde contém a estrutura

---
+++ src/map/Ordenacao/Evento.java
@@ -0,0 +1,34 @@
+package map.Ordenacao;
+
+import java.util.Collections;
+
+public class Evento {
+    //atributos
+    private String nome;
+
+    private String atracao;
+
+    //construtor
+
+    public Evento(String nome, String atracao) {
+        this.nome = nome;
+        this.atracao = atracao;
+    }
+
+    //getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getAtracao() {
+        return atracao;
+    }
+
+    @Override
+    public String toString() {
+        return "Evento{" +
+                "nome='" + nome + '\'' +
+                ", atracao='" + atracao + '\'' +
+                '}';
+    }
+}
